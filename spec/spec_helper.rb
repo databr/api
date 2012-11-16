e6029619ae812d15d9492fb0583c256aedbc5f50
Duke <duke@riseup.net>
2012-11-16 04:32:56 -0200

    install rspec

---
+++ spec/spec_helper.rb
@@ -0,0 +1,22 @@
+require 'bundler'
+Bundler.require
+
+require 'simple_cov'
+require 'pry'
+
+SimpleCov.start
+
+Dir[File.join("data/*.rb")].each {|f| require File.absolute_path(f) }
+Dir[File.join("spec/support/*.rb")].each {|f| require File.absolute_path(f) }
+
+RSpec.configure do |config|
+  config.treat_symbols_as_metadata_keys_with_true_values = true
+  config.run_all_when_everything_filtered = true
+  config.filter_run :focus
+
+  config.order = 'random'
+
+  config.expect_with :rspec do |c|
+    c.syntax = :expect
+  end
+end
