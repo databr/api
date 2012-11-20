07da665b9542528de548ad69072107eb33d58ea4
Duke <duke@vertigem.xxx>
2012-11-20 15:52:19 -0200

    fixed activerecord config

--- lib/activerecord.rb
+++ lib/activerecord.rb
@@ -5,4 +5,4 @@ require 'active_record'
 Dir[File.join("lib/models/*.rb")].each {|f| require File.absolute_path(f) }
 
 
-ActiveRecord::Base.establish_connection(YAML.load(File.read(File.join('db','config.yml')))[ENV['ENV'] ? ENV['ENV'] : 'development'])
+ActiveRecord::Base.establish_connection(YAML.load(File.read(File.join('config','database.yml')))[ENV['RACK_ENV'] ? ENV['RACK_ENV'] : 'development'])
