a29eef8c2d99085c4d61ea40849bdb2a7bba1bd2
Duke <duke@vertigem.xxx>
2012-11-21 14:24:52 -0200

    parser database.yml if env is production

--- lib/activerecord.rb
+++ lib/activerecord.rb
@@ -4,5 +4,16 @@ require 'active_record'
 
 Dir[File.join("lib/models/*.rb")].each {|f| require File.absolute_path(f) }
 
+env = ENV['RACK_ENV'] ? ENV['RACK_ENV'] : 'development'
 
-ActiveRecord::Base.establish_connection(YAML.load(File.read(File.join('config','database.yml')))[ENV['RACK_ENV'] ? ENV['RACK_ENV'] : 'development'])
+database_config_file = File.read('config/database.yml')
+
+if env == 'production'
+  e = Erubis::Eruby.new(database_config_file)
+  database_config_file = e.result(binding())
+end
+
+database_config = YAML.load(database_config_file)[env]
+
+ActiveRecord::Base.establish_connection(database_config)
+
