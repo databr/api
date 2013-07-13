39757082ffbaf7eb9573081dcd3f9d9305794309
Duke <duke@vertigem.xxx>
2013-07-13 15:27:01 -0300

    updated schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,31 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121122040505) do
+ActiveRecord::Schema.define(:version => 20130713141000) do
+
+  create_table "cotas", :force => true do |t|
+    t.string  "deputado_id"
+    t.string  "carteira_parlamentar"
+    t.string  "legislatura"
+    t.string  "uf"
+    t.string  "partido"
+    t.string  "codigo_legislatura"
+    t.string  "sub_cota"
+    t.string  "descricao"
+    t.string  "beneficiario"
+    t.string  "cnpjcpf"
+    t.string  "numero"
+    t.string  "tipo_documento"
+    t.string  "data_emissao"
+    t.decimal "valor_documento",      :precision => 10, :scale => 2
+    t.decimal "valor_glossa",         :precision => 10, :scale => 2
+    t.decimal "valor_liquido",        :precision => 10, :scale => 2
+    t.integer "mes"
+    t.integer "ano"
+    t.string  "parcela"
+    t.string  "lote"
+    t.string  "ressarcimento"
+  end
 
   create_table "deputados", :force => true do |t|
     t.string  "nome"
