a6e1f76a8a7f05c358c5dac44548162ef6ca4c53
Duke <duke@vertigem.xxx>
2013-07-15 15:03:26 -0300

    update cota_crawler_service to read a zip file

--- lib/services/cota_crawler_service.rb
+++ lib/services/cota_crawler_service.rb
@@ -1,15 +1,29 @@
 class CotaCrawlerService
   def self.save_from_cota_xml_parser
-    file = 'db/data/cota_2013.xml'
-    parser = CotaXMLParser.new(file)
-    parser.cotas.each do |cota|
-      deputado = Deputado.where(nome_parlamentar: cota[:nome_parlamentar]).first
-      if deputado.nil?
-        File.open('deputado_not_found', 'a') {|f| f.puts(cota) }
-        next
+    unzip('db/data/cota_2013.xml.zip') do |file|
+      parser = CotaXMLParser.new(file)
+      parser.cotas.each do |cota|
+        deputado = Deputado.where(nome_parlamentar: cota[:nome_parlamentar]).first
+        if deputado.nil?
+          File.open('deputado_not_found', 'a') {|f| f.puts(cota) }
+          next
+        end
+        cota.delete(:nome_parlamentar)
+        deputado.cotas.create! cota
       end
-      cota.delete(:nome_parlamentar)
-      deputado.cotas.create! cota
     end
+  end
+
+  def self.unzip(file, &block)
+    require 'zip/zipfilesystem'
+    xml_file = file.gsub('.zip', '')
+    entry = xml_file.split("/").last
+    dest = "tmp/#{entry}"
+
+    Zip::ZipFile.open(file) do |zipfile|
+      zipfile.extract(entry, dest)
+    end
+
+    yield(dest)
   end
 end
