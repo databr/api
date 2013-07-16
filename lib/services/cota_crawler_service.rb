2c167a7d171c53a35bde25d7f0ae9b03296517b4
Duke <duke@vertigem.xxx>
2013-07-16 00:49:51 -0300

    updated cota_crawler_service

--- lib/services/cota_crawler_service.rb
+++ lib/services/cota_crawler_service.rb
@@ -1,15 +1,19 @@
+# encoding: utf-8
+#
 class CotaCrawlerService
   def self.save_from_cota_xml_parser
     unzip('db/data/cota_2013.xml.zip') do |file|
       parser = CotaXMLParser.new(file)
       parser.cotas.each do |cota|
         deputado = Deputado.where(nome_parlamentar: cota[:nome_parlamentar]).first
-        if deputado.nil?
-          File.open('deputado_not_found', 'a') {|f| f.puts(cota) }
-          next
-        end
+        print "Creating cota..."
         cota.delete(:nome_parlamentar)
-        deputado.cotas.create! cota
+        if deputado.nil?
+          Cota.create! cota
+        else
+          deputado.cotas.create! cota
+        end
+        puts " Done!"
       end
     end
   end
@@ -20,6 +24,7 @@ class CotaCrawlerService
     entry = xml_file.split("/").last
     dest = "tmp/#{entry}"
 
+    system "rm #{dest}"
     Zip::ZipFile.open(file) do |zipfile|
       zipfile.extract(entry, dest)
     end
