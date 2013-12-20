1e854a6f159036fea6302e77b33abe9728c0d899
Duke <duke@riseup.net>
2013-12-20 02:58:25 -0200

    fixed image_url to new deputados

--- lib/services/deputado_image_service.rb
+++ lib/services/deputado_image_service.rb
@@ -5,5 +5,8 @@ class DeputadoImageService
       deputado = Deputado.find_by_parlamentar_id(deputado_data["id_dep"])
       deputado.update_attribute :image_url, deputado_data["image_urls"].first if deputado
     end
+    Deputado.where(image_url: nil).each do |deputado|
+      deputado.update_attribute :image_url,  "http://www.camara.gov.br/internet/deputado/bandep/#{deputado.cadastro_id}.jpg"
+    end
   end
 end
